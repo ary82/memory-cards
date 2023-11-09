1290d52c5679a5be358b1a8f456160195d6ffa08
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-11-09 18:42:55 +0530

    add interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import CardList from "./CardList.jsx";
-import "./styles/main.scss"
+import Welcome from "./Welcome.jsx";
+import "./styles/main.scss";
+import { useState } from "react";
 
 function App() {
+  // Game States. 0 = welcome, 1 = easy mode, 2 = medium mode, 3 = hard mode
+  const [game, setgame] = useState(0);
   return (
     <>
-      <h1 className="title">Memory Game</h1>
-      <CardList num="5" />
+      {console.log(game)}
+      {game === 0
+        ? (
+          <Welcome
+            easy={() => setgame(1)}
+            medium={() => setgame(2)}
+            hard={() => setgame(3)}
+          />
+        )
+        : (
+          <>
+            <h1 className="title">Memory Game</h1>
+            <CardList num={game * 5} />
+          </>
+        )}
     </>
   );
 }
